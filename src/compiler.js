aeaa435cde6dd1aa309d00e51b1d982e6fcba33c
bucket3432 <bucket3432@disroot.org>
2022-02-26 04:14:20 -0500

    Compile "template" template blocks
    There is no explicit check for "template" in the Effect field. Rather,
    this is what the flow will be for any non-"code" template blocks.
    Processing "code" blocks will be done in a follow-up commit.
    
    On the input, the following flow is followed:
    
     1. Remove comments
     2. Extract blocks
     3. Process blocks
        3.1. Parse block
        3.2. Minify text
        3.3. Format as ASS event
     4. Recombine
    
    Removing comments first allows the block delimiters to be commented out.
    
    The same flow will be followed for "code" template blocks, but with
    a different minifying function for step 3.2.

--- src/compiler.js
+++ src/compiler.js
@@ -1,5 +1,29 @@
+const { pipe } = require('./utils');
+const {
+  extractTemplateBlocks,
+  removeComments,
+  parseTemplateBlock,
+} = require('./parse');
+const {
+  formatEvent,
+  minifyASSText,
+  minifyLua,
+  minifyTemplate,
+} = require('./format');
+
+const processBlock = pipe(
+  parseTemplateBlock,
+  ({ text, ...event }) => ({ ...event, text: minifyTemplate(text) }),
+  formatEvent
+);
+
 function compile(input) {
-  return '';
+  return pipe(
+    removeComments,
+    extractTemplateBlocks,
+    (blocks) => blocks.map(processBlock),
+    (lines) => lines.join('')
+  )(input);
 };
 
 module.exports = {
--- test/compiler.test.js
+++ test/compiler.test.js
@@ -10,4 +10,28 @@ describe('compile', function() {
 
     assert.equal(actual, expected);
   });
+
+  it('should compile one template block', function() {
+    const input = String.raw`Outside 1
+%[ template line ^ Comment @ 1 < 0:12:34.567 > 1:23:45.678 $ Alternate # Actor =L 10 =R 20 =V 30
+  { %; Comment in override
+    \pos(!
+      $x
+        + 10 -- Add 10
+    !,! %; Comment in inline code
+      $y
+        + 20
+    !)
+    \c&H000000&
+  }
+  %; %] Comment containing end marker
+  Line: %; Comment outside override
+%] Outside 2
+`;
+    const expected = String.raw`Comment: 1,0:12:34.567,1:23:45.678,Alternate,Actor,10,20,30,template line,{\pos(!$x+10!,!$y+20!)\c&H000000&}Line:`;
+
+    const actual = compile(input);
+
+    assert.equal(actual, expected);
+  });
 });
