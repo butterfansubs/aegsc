5cc44a36cf37ff84769c50d3d59624c9cba8dfbe
bucket3432 <bucket3432@disroot.org>
2022-03-10 01:53:20 -0500

    Supply defaults for missing fields in ASS objects
    This implementation uses Object.assign, which means only own properties
    will be carried forward and properties that are present but undefined
    will remain undefined. The original object is not modified.

--- src/defaults.js
+++ src/defaults.js
@@ -1,13 +1,17 @@
-function supplyDefaults() {
-  return {
-      layer: 0,
-      startTime: '0:00:00.00',
-      endTime: '0:00:00.00',
-      style: 'Default',
-      actor: '',
-      effect: '',
-      text: '',
-    };
+function supplyDefaults(assObject) {
+  return Object.assign({
+    type: 'Comment',
+    layer: '0',
+    startTime: '0:00:00.00',
+    endTime: '0:00:00.00',
+    style: 'Default',
+    actor: '',
+    marginL: '0',
+    marginR: '0',
+    marginV: '0',
+    effect: '',
+    text: '',
+  }, assObject);
 }
 
 module.exports = {
--- test/defaults.test.js
+++ test/defaults.test.js
@@ -4,16 +4,46 @@ const { supplyDefaults } = require('../src/defaults');
 describe('supplyDefaults', function() {
   it('should return defaults when no arguments are provided', function() {
     const expected = {
-      layer: 0,
+      type: 'Comment',
+      layer: '0',
       startTime: '0:00:00.00',
       endTime: '0:00:00.00',
       style: 'Default',
       actor: '',
+      marginL: '0',
+      marginR: '0',
+      marginV: '0',
       effect: '',
       text: '',
     };
 
     const actual = supplyDefaults();
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should supply defaults for missing fields', function() {
+    const input = {
+      layer: '1',
+      actor: 'Name',
+      effect: 'template line',
+      text: '{}',
+    };
+    const expected = {
+      type: 'Comment',
+      layer: '1',
+      startTime: '0:00:00.00',
+      endTime: '0:00:00.00',
+      style: 'Default',
+      actor: 'Name',
+      marginL: '0',
+      marginR: '0',
+      marginV: '0',
+      effect: 'template line',
+      text: '{}',
+    };
+
+    const actual = supplyDefaults(input);
 
     assert.deepEqual(actual, expected);
   });
